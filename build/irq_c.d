a823a52d6033a38011185e5cf766b79b47d56665
Johnny Drumgole <85814044+jdrumgole@users.noreply.github.com>
2025-03-22 14:54:44 -0400

    Add files via upload

---
+++ build/irq_c.d
@@ -0,0 +1,3 @@
+build/irq_c.o: src/irq.c include/utils.h include/printf.h include/timer.h \
+ include/entry.h include/mini_uart.h include/peripherals/mini_uart.h \
+ include/peripherals/base.h include/peripherals/irq.h
